6c3fb26d86d734baca99d8e566b24fb10abd6a45
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2024-05-22 12:27:38 -0500

    Delete booklingo-master directory

--- booklingo-master/api/utils/catchError.js
+++
@@ -1,5 +0,0 @@
-const catchError = (fun) => (req, res, next) => {
-    Promise.resolve(fun(req, res, next)).catch((error) => next(error));
-};
-
-module.exports = catchError;
--- booklingo-master/scripts/createSecret.js
+++
@@ -1,5 +0,0 @@
-// Create the secret fot jwt
-
-const crypto = require("crypto");
-
-console.log(crypto.randomBytes(32).toString("hex"));
